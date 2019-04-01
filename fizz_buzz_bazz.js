b6b14d5dd455330e8c14c2409beffcde71157067
Julian Vargas <julianvargasalvarez@gmail.com>
2019-04-01 08:39:11 -0500

    Intitial commit with all tests passing

---
+++ fizz_buzz_bazz.js
@@ -0,0 +1,28 @@
+function assertEqual(description, expected, actual) {
+  if(expected===actual){
+    console.log(description, ":", "OK")
+  }
+  else {
+    console.log(description, ": F", expected, "was expected but got", actual)
+  }
+}
+
+function fizz_buzz_bazz(number) {
+  if(number % 3 === 0 && number % 5 === 0) {
+    return 'Bazz'
+  }
+  if(number % 3 === 0) {
+    return 'Fizz'
+  }
+  if(number % 5 === 0) {
+    return 'Buzz'
+  }
+  return number
+}
+
+assertEqual("Returns 'Fizz' when 3 is given", "Fizz", fizz_buzz_bazz(3))
+assertEqual("Returns 'Buzz' when 5 is given", "Buzz", fizz_buzz_bazz(5))
+assertEqual("Returns 'Bazz' when 15 is given", "Bazz", fizz_buzz_bazz(15))
+assertEqual("Returns 4 when 4 is given", 4, fizz_buzz_bazz(4))
+
+
